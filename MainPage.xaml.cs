a57b666c25f0e18f68cf0fa863ce570fa6601b46
Abinesh p <abinesh.palanisamy@syncfusion.com>
2025-07-09 17:51:00 +0530

    Blog changes

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace MAUI_Sync_DataGrid
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+        private void SfDataGrid_Loaded(object sender, EventArgs e)
+        {
+            dataGrid.View.Filter = FilterRecords;
+            //dataGrid.View.RefreshFilter();
+        }
+        public bool FilterRecords(object record)
+        {
+            TemplateModel? templateModel = record as TemplateModel;
+
+            if (templateModel != null && templateModel.DealerName == "Jefferson")
+            {
+                return true;
+            }
+            return false;
+        }
+
+       
+    }
+
+}
